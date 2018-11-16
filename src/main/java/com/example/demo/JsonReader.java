e8ee3e0585f4107f8290649b0038b66eca9f9269
greenery <elindreams@hanmail.net>
2018-11-16 15:32:47 +0900

    .

--- src/main/java/com/example/demo/JsonReader.java
+++ src/main/java/com/example/demo/JsonReader.java
@@ -9,59 +9,86 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.example.demo.vo.Food;
+import com.example.demo.vo.Incredient;
 import com.google.gson.Gson;
 
 public class JsonReader {
 
-	 private static String readAll(Reader rd) throws IOException {
-		    StringBuilder sb = new StringBuilder();
-		    int cp;
-		    while ((cp = rd.read()) != -1) {
-		      sb.append((char) cp);
-		    }
-		    return sb.toString();
-		  }
+	private static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		return sb.toString();
+	}
 
-		  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-		    InputStream is = new URL(url).openStream();
-		    try {
-		      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-		      String jsonText = readAll(rd);
-		      JSONObject json = new JSONObject(jsonText);
-		      return json;
-		    } finally {
-		      is.close();
-		    }
-		  }
+	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+		InputStream is = new URL(url).openStream();
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JSONObject json = new JSONObject(jsonText);
+			return json;
+		} finally {
+			is.close();
+		}
+	}
 
-		  public static void main(String[] args) throws IOException, JSONException {
-		    JSONObject json = readJsonFromUrl("http://openapi.foodsafetykorea.go.kr/api/575b452f24504f6f983e/COOKRCP01/json/1/10");
-		    System.out.println(json.get("COOKRCP01"));
+	public static void main(String[] args) throws IOException, JSONException {
+		JSONObject json = readJsonFromUrl(
+				"http://openapi.foodsafetykorea.go.kr/api/575b452f24504f6f983e/COOKRCP01/json/1/10");
+		System.out.println(json.get("COOKRCP01"));
 
-	        Gson gson = new Gson();
-	        Cookrcp01 food = gson.fromJson(json.get("COOKRCP01").toString(), Cookrcp01.class);
-	        
-	        List<Row> rows = food.getRow();
-	        
-	        System.out.println("row size: "+rows.size());
-	        
-	        for(int i=0; i< rows.size(); i++) {
-	        	String[] incredients = rows.get(i).getRCP_PARTS_DTLS().split(",");
-	        	
-//	        	for(String incredient : incredients){
-//	        		System.out.println(incredient.trim());
-//	        	}
-	        	
-	        }
-	        
-	        //System.out.println(food.toString());
-	        //만드는법 
+		Gson gson = new Gson();
+		Cookrcp01 cookrcp = gson.fromJson(json.get("COOKRCP01").toString(), Cookrcp01.class);
 
-		  }
+		List<Row> rows = cookrcp.getRow();
 
+		System.out.println("row size: " + rows.size());
+
+		List<Food> foodList = new ArrayList<Food>(); // food list
+
+		for (int i = 0; i < rows.size(); i++) {
+
+			Food food = new Food(); // food 하나
+
+			food.setHashTag(rows.get(i).getHASHTAG()); // hashtag
+			food.setFat(Float.parseFloat(rows.get(i).getINFO_FAT())); // fat
+			food.setCarbohydrate(Float.parseFloat(rows.get(i).getINFO_CAR())); // 탄수화물
+			food.setName(rows.get(i).getRCP_NM()); // 메뉴명
+			food.setProtein(Float.parseFloat(rows.get(i).getINFO_PRO())); // 단백질
+			food.setCalorie(Float.parseFloat(rows.get(i).getINFO_ENG())); // 열량
+			food.setIngredients(rows.get(i).getRCP_PARTS_DTLS()); // 재료정보
+
+			food.setSmallImageLocation(rows.get(i).getATT_FILE_NO_MAIN()); // 이미지 소
+			food.setBigImageLocation(rows.get(i).getATT_FILE_NO_MK()); // 이미지 대
+
+			// 재료 낱개
+			String[] incredients = rows.get(i).getRCP_PARTS_DTLS().split(",");
+			
+			List<Incredient> incredientList = new ArrayList<Incredient>(); // 재료 리스트
+			for (String indi : incredients) {
+				Incredient incredient = new Incredient();
+				incredient.setName(indi.trim());
+				incredientList.add(incredient); // 재료리스트에 재료 낱개로 넣기
+			}
+
+			rows.get(i).getMANUAL01(); // 만드는법 설명
+			rows.get(i).getMANUAL_IMG01(); // 만드는 법 이미지
+
+			foodList.add(food); // food list에 food 넣기
+		}
+		
+
+	}
+	
+	
 }
--- src/main/java/com/example/demo/Row.java
+++ src/main/java/com/example/demo/Row.java
@@ -5,72 +5,64 @@ import lombok.Data;
 @Data
 public class Row {
 	String INFO_WGT;
-	String MANUAL01;
-	String MANUAL02;
 	String INFO_ENG;
-	String MANUAL03;
 	String ATT_FILE_NO_MK;
-
+	String RCP_PARTS_DTLS;
+	
+	String MANUAL01;
+	String MANUAL02;	
+	String MANUAL03;
+	String MANUAL04;
+	String MANUAL05;
 	String MANUAL06;
 	String MANUAL07;
 	String MANUAL08;
 	String MANUAL09;
-	
-	String RCP_PARTS_DTLS;
-	String MANUAL_IMG01;
-	
-
-	String INFO_PRO;
-	String RCP_NM;
-	String MANUAL11;
-	String MANUAL_IMG03;
-
-	String MANUAL12;
-	String INFO_CAR;
-	
-	String MANUAL_IMG02;
-	String MANUAL_IMG05;
-	
 	String MANUAL10;
-	String MANUAL_IMG04;
-	
-	String RCP_WAY2;
-	
-	String MANUAL15;
-	String MANUAL16;
+	String MANUAL11;
+	String MANUAL12;
 	String MANUAL13;
 	String MANUAL14;
-	
-	String MANUAL19;
+	String MANUAL15;
+	String MANUAL16;
 	String MANUAL17;
 	String MANUAL18;
+	String MANUAL19;
+	String MANUAL20;
 	
+	String INFO_PRO;
+	String RCP_NM;
+	String MANUAL_IMG03;
+
+	String INFO_CAR;
+
+	String RCP_WAY2;
 
 	String RCP_PAT2;
 	String HASHTAG;
-	String MANUAL_IMG06;
 	
+	String MANUAL_IMG01;
+	String MANUAL_IMG02;
+	String MANUAL_IMG04;
+	String MANUAL_IMG05;
+	String MANUAL_IMG06;
 	String MANUAL_IMG07;
 	String MANUAL_IMG08;
 	String MANUAL_IMG09;
-
-	String RCP_SEQ;
-	
-	String MANUAL_IMG16;
-	String MANUAL_IMG15;
-	String MANUAL_IMG14;
-	String MANUAL_IMG13;
-	String MANUAL_IMG12;
-	String MANUAL_IMG11;
 	String MANUAL_IMG10;
-	
-	String MANUAL20;
-	String MANUAL_IMG19;
+	String MANUAL_IMG11;
+	String MANUAL_IMG12;
+	String MANUAL_IMG13;
+	String MANUAL_IMG14;
+	String MANUAL_IMG15;
+	String MANUAL_IMG16;
 	String MANUAL_IMG17;
 	String MANUAL_IMG18;
-	
-	String ATT_FILE_NO_MAIN;
+	String MANUAL_IMG19;
 	String MANUAL_IMG20;
+	
+	String RCP_SEQ;
+	String ATT_FILE_NO_MAIN;
 	String INFO_NA;
 	String INFO_FAT;
 	
